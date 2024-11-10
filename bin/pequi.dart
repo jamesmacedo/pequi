4b2e9fbdefe71b1f6306f47b45470d48386dd116
James Yan <jamesyancm@gmail.com>
2024-11-10 01:10:28 -0300

    feat: Check if the symbolic link already exists in the application
    feat: Search for the target file absolute path before create the symbolic link

--- bin/pequi.dart
+++ bin/pequi.dart
@@ -4,34 +4,40 @@ import 'package:args/args.dart';
 
 const String filePath = 'environments.yaml';
 const String baseDirectory = 'environments';
-const Map<String, String> neededFolders = {'assets':'assets','config':'lib/config'};
+const Map<String, String> neededFolders = {'assets': 'assets', 'config': 'lib/config'};
 
 Future<bool> checkDirectory(String path) async {
   final dir = Directory(path);
   return await dir.exists();
 }
 
-
 Future<void> createSymbolicLink(String targetPath, String linkPath) async {
   try {
-    if (!await File(targetPath).exists() && !await Directory(targetPath).exists()) {
-      throw Exception('The target path does not exists: $targetPath');
+    final absoluteTargetPath = Directory(targetPath).absolute.path;
+    final link = Link(linkPath);
+
+    if (!await Directory(targetPath).exists() && !await File(targetPath).exists()) {
+      throw Exception('The target path does not exist: $absoluteTargetPath');
     }
 
-    final link = Link(linkPath);
-    await link.create(targetPath);
-    print('Created symbolic link: $linkPath -> $targetPath');
+    //check if the symbolic link exists
+    if (await link.exists()) {
+      await link.delete();
+      print('Removed existing symbolic link: $linkPath');
+    }
+
+    await link.create(absoluteTargetPath);
+    print('Created symbolic link: $linkPath -> $absoluteTargetPath');
   } catch (e) {
-    print('Error while trying to created the symbolic link: $e');
+    print('Error while trying to create the symbolic link: $e');
   }
 }
 
 Future<void> main(List<String> arguments) async {
-
   if (await checkDirectory('environments') == false) {
-    print('The environments folder does not exists in this project, please create one');
+    print('The environments folder does not exist in this project, please create one');
     return;
-  } 
+  }
 
   final file = File(filePath);
   if (!await file.exists()) {
@@ -46,23 +52,23 @@ Future<void> main(List<String> arguments) async {
 
   String keys = data['environments'].keys.toList().join(', ');
 
-  parser.addOption('environment', abbr: 'e',  help: 'Enter the available company codes: [$keys]');
+  parser.addOption('environment', abbr: 'e', help: 'Enter the available company codes: [$keys]');
 
   final args = parser.parse(arguments);
   final environment = args['environment'];
 
-  if(arguments.isEmpty){
+  if (arguments.isEmpty) {
     print('Please enter one of the following available company codes: [$keys]');
     return;
   }
 
-  if(environment == null){
+  if (environment == null) {
     print('Please enter one of the following available company codes using the -e flag: [$keys]');
     return;
   }
 
   if (!data['environments'].containsKey(environment)) {
-    print('Enviroment "$environment" does not exists.');
+    print('Environment "$environment" does not exist.');
     return;
   }
 
@@ -71,6 +77,5 @@ Future<void> main(List<String> arguments) async {
   for (var folderKey in neededFolders.keys.toList()) {
     await createSymbolicLink('$baseDirectory/$configPath/$folderKey', neededFolders[folderKey]!);
   }
-
-  // await createSymbolicLink(configPath, baseDirectory, 'config_$environment');
 }
+
